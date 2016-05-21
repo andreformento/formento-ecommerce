1ae130452a8ac8996d4cf49f7358c60865dd9b78
André Formento <andreformento.sc@gmail.com>
2016-05-21 04:30:52 -0300

    anothers fixes

---
+++ src/main/resources/static/order/order.controller.js
@@ -0,0 +1,102 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('ShoppingCartController', ShoppingCartController);
+
+    ShoppingCartController.$inject = ['ShoppingCartService', 'AuthenticationService', 'FlashService', '$rootScope', '$translate', '$translatePartialLoader'];
+    function ShoppingCartController(ShoppingCartService, AuthenticationService, FlashService, $rootScope, $translate, $translatePartialLoader) {
+        $translatePartialLoader.addPart('system');
+        $translatePartialLoader.addPart('exception');
+        $translatePartialLoader.addPart('shoppingCart');
+        $translate.refresh();
+
+        var vm = this;
+
+        vm.allItemShoppingCarts = [];
+
+        initController();
+
+        function initController() {
+            loadAllItemShoppingCarts();
+        }
+
+        function loadAllItemShoppingCarts() {
+            ShoppingCartService.GetAll()
+                .then(function (response) {
+                    vm.allItemShoppingCarts = response._embedded && response._embedded.itemShoppingCarts ? response._embedded.itemShoppingCarts : [];
+                },
+                function (response) {
+                    FlashService.Error(response.message);
+                });
+        }
+
+        vm.remove = function(itemShoppingCart) {
+            console.log('itemShoppingCart',itemShoppingCart);
+
+            ShoppingCartService.RemoveById(itemShoppingCart.id)
+                .then(function (response) {
+                    FlashService.Success($translate.instant('shoppingCart.removedItemFromCart'), true);
+                    loadAllItemShoppingCarts();
+                },
+                function (response) {
+                    FlashService.Error(response.message);
+                });
+        }
+
+        vm.add = function(itemShoppingCart) {
+            console.log('add',itemShoppingCart);
+
+            ShoppingCartService.Add(itemShoppingCart.id)
+                .then(function (response) {
+                    FlashService.Success($translate.instant('shoppingCart.addItemToCart'), true);
+                    loadAllItemShoppingCarts();
+                },
+                function (response) {
+                    FlashService.Error(response.message);
+                });
+        }
+
+        vm.plus = function(itemShoppingCart) {
+            console.log('plus',itemShoppingCart);
+
+            ShoppingCartService.Plus(itemShoppingCart.id)
+                .then(function (response) {
+                console.log('ok',response);
+                    if (response.quantity) {
+                        FlashService.Success($translate.instant('shoppingCart.addItemToCart'), true);
+                        loadAllItemShoppingCarts();
+                    } else if (itemShoppingCart.message) {
+                        FlashService.Error($translate.instant('itemShoppingCart.cannotAddItem'));
+                    } else {
+                        FlashService.Error(response.message);
+                    }
+                },
+                function (response) {
+                console.log('erro',response);
+                    FlashService.Error(response.message);
+                });
+        }
+
+        vm.finalizeCurrentFromUser = function(itemShoppingCart) {
+            ShoppingCartService.finalizeCurrentFromUser()
+                .then(function (response) {
+                    if (response.quantity) {
+                        FlashService.Success($translate.instant('shoppingCart.createdOrder'), true);
+                        $location.path('/order');
+                    } else if (itemShoppingCart.message) {
+                        FlashService.Error($translate.instant('shoppingCart.cannotCreateOrder'));
+                    } else {
+                        FlashService.Error(response.message);
+                    }
+                },
+                function (response) {
+                console.log('erro',response);
+                    FlashService.Error(response.message);
+                });
+        }
+
+    }
+
+})();
